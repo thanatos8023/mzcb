e59ffd5a2e64c2fa36aeec2adc17b29867d6ce67
thanatos8023 <thanatos8023@gmail.com>
2019-03-18 15:28:50 +0900

    menu bar fixed in simulator

--- simulator/public/js/func.js
+++ simulator/public/js/func.js
@@ -1,21 +1,24 @@
 $(document).ready(function(){
-
-
  	/*메뉴*/
- 	$('.sub>ul>li>a').on('click',function(){
- 		if($(this).next().hasClass('show')){
- 			$('.sub>ul>li>ul').removeClass('show').slideUp();
- 			
+	$('.sub-list>li>a').on('click',function(){
+		if ($(this).parent().hasClass('active')) {
+			$(this).parent().removeClass('active');
+		}else{
+			$(this).parent().siblings().removeClass('active');
+			$(this).parent().addClass('active');
+		}
+		 
+		if($(this).next().hasClass('show')){
+			$('.sub-list>li>ul').removeClass('show').slideUp();
  		}else{
- 			$('.sub>ul>li>ul').removeClass('show').stop().slideUp();	
+			$('.sub-list>li>ul').removeClass('show').stop().slideUp();	
  			$(this).next().addClass('show').stop().slideDown();
- 		}
- 		
- 		
- 	});
+		 }
+	 });
 
- 	$('.sub>ul>li>ul>li>a').on('click',function(){
- 		$(this).parent().siblings().removeClass('active');
- 		$(this).parent().addClass('active');
- 	});
+	(function ($) {
+		$(window).on("load", function () {
+			$(".sub").mCustomScrollbar();
+		});
+	})(jQuery);
 });
